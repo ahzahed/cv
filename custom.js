508641e7870cbeee4dd989e3343376bca71a7585
Amir Hamza <ah.zahed00@gmail.com>
2020-01-05 21:53:27 +0600

    CV with html/css not responsive

---
+++ custom.js
@@ -0,0 +1,74 @@
+var about = {
+  name:"Md. Amir Hamza",
+  dob:"12/10/1996, Bangladesh",
+  add:"Moddho Paikpara",
+  phone:"01521331041",
+  email:"ah.zahed00@gmail.com",
+}
+document.getElementById("name").innerHTML=about.name;
+document.getElementById("dob").innerHTML=about.dob;
+document.getElementById("add").innerHTML=about.add;
+document.getElementById("phone").innerHTML=about.phone;
+document.getElementById("email").innerHTML=about.email;
+
+var summary = {
+  sum:"I believe my hard working that will help me to reach my ultimate goal and I want to enrich my Interpersonal Information skill and prove myself as an asset to the organization by devoting my creativity and knowledge."
+}
+document.getElementById("sum").innerHTML=summary.sum;
+
+var skills = {
+  a:"C/C++, PHP 7, JavaScript, HTML5, CSS3, Java(basic)",
+  b:"Laravel 6, Bootstrap 4, Wordpress, Reactjs",
+  c:"MySQLi",
+  d:"Blender, Photoshop, Maya(basic)",
+  e:"MS Word, Powerpoint, MS Excel",
+  f:"Camtasia Studio, Adobe Premiere Pro, After Effect",
+}
+document.getElementById("a").innerHTML=skills.a;
+document.getElementById("b").innerHTML=skills.b;
+document.getElementById("c").innerHTML=skills.c;
+document.getElementById("d").innerHTML=skills.d;
+document.getElementById("e").innerHTML=skills.e;
+document.getElementById("f").innerHTML=skills.f;
+
+var problem = {
+  aa:"I solved about 100+ problems in the different online judge such as Uri Online Judge, Codeforces, Uva on-line judge.",
+  bb:"I have completed HTML, CSS, C++, PHP all challenges in “Solo Learn”.",
+  cc:"My University team name is EU Coder. My team already participated in 2 Preliminary national level contests and 3 Intra-University contests. In 2 Intra-University contests, our team got a good position.",
+}
+document.getElementById("aa").innerHTML=problem.aa;
+document.getElementById("bb").innerHTML=problem.bb;
+document.getElementById("cc").innerHTML=problem.cc;
+
+
+var edu = {
+  deg:["B.SC in CSE","HSC","SSC"],
+  uni:["Eastern University","Gov't Science College","Tammirul Millat Kamil"],
+  res:["Running","4.42 out of 5.00","5.00 out of 5.00"],
+  year:["2015","2013"]
+}
+document.getElementById("deg1").innerHTML=edu.deg[0];
+document.getElementById("deg2").innerHTML=edu.deg[1];
+document.getElementById("deg3").innerHTML=edu.deg[2];
+document.getElementById("uni1").innerHTML=edu.uni[0];
+document.getElementById("uni2").innerHTML=edu.uni[1];
+document.getElementById("uni3").innerHTML=edu.uni[2];
+document.getElementById("res1").innerHTML=edu.res[0];
+document.getElementById("res2").innerHTML=edu.res[1];
+document.getElementById("res3").innerHTML=edu.res[2];
+document.getElementById("year1").innerHTML=edu.year[0];
+document.getElementById("year2").innerHTML=edu.year[1];
+
+
+var course = {
+  v:"Creative It - Professional Web Development",
+  w:"It Bari - Web Design, Wordpress",
+  x:"Reactjs - Rabbil Hassan, Twinkle Cats",
+  y:"Laravel - Learn Hunter",
+  z:"Udemy - Bootstrap 4, PSD to HTML",
+}
+document.getElementById("v").innerHTML=course.v;
+document.getElementById("w").innerHTML=course.w;
+document.getElementById("x").innerHTML=course.x;
+document.getElementById("y").innerHTML=course.y;
+document.getElementById("z").innerHTML=course.z;
